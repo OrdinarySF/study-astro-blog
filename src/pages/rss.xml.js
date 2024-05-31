67368ab1621ed8d5187d74902ab8c69ce58cfda2
Ordinary <ordinary@zjgsdx.edu.cn>
2024-05-31 12:06:00 +0800

    feat: use content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+    const posts = await getCollection("posts");
     return rss({
         title: 'Astro 学习 | 博客',
         description: '我的学习 Astro 之路',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/posts/${post.slug}/`,
+        })),
         customData: `<language>zh-cn</language>`,
     });
 }
